12ba14f1992f9cc95e5bbfca26060c2149e15f8e
James C <3661681+JamesMCo@users.noreply.github.com>
2018-12-01 16:14:13 +0000

    Added logging for server

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import colorama
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
@@ -6,18 +7,24 @@ connections = []
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
+        global connections
+
+        print(f"{colorama.Fore.RESET}[{colorama.Fore.RED}SRVR{colorama.Fore.RESET}] {colorama.Fore.YELLOW}Opened new connection...{colorama.Fore.RESET}")
         connections.append(self)
         self.set_nodelay(True)
 
     def on_message(self, message):
+        global connections
+
+        print(f"{colorama.Fore.RESET}[{colorama.Fore.RED}SRVR{colorama.Fore.RESET}] {colorama.Fore.YELLOW}Recieved (and broadcasting) new message...{colorama.Fore.RESET}")
         for connection in connections:
             try:
-                connection.write_message(s)
+                connection.write_message(message)
             except Exception as e:
-                pass
+                print(f"{colorama.Fore.RESET}[{colorama.Fore.RED}SRVR{colorama.Fore.RESET}] {colorama.Fore.YELLOW}Had an exception while sending message: {colorama.Fore.RESET} {e}")
 
     def on_close(self):
-        to_remove.append(connections.index(self))
+        print(f"{colorama.Fore.RESET}[{colorama.Fore.RED}SRVR{colorama.Fore.RESET}] {colorama.Fore.YELLOW}Closing a connection...{colorama.Fore.RESET}")
 
     def check_origin(self, origin):
         return True
@@ -26,5 +33,6 @@ if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", WebSocketHandler),
     ])
-    app.listen(4312)
+    app.listen(80)
+    print(f"{colorama.Fore.RESET}[{colorama.Fore.RED}SRVR{colorama.Fore.RESET}] {colorama.Fore.YELLOW}Starting WebSocket server...{colorama.Fore.RESET}")
     tornado.ioloop.IOLoop.current().start()
